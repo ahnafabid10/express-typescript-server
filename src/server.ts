24b8f1e8941fcd7a6a7d17f97fc66428f5e9b2e0
Ahnaf Abid <ahnafabid600@gmail.com>
2026-06-06 22:46:45 +0600

    Refactor configuration to use environment variables for connection string and port

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express, { type Application, type Request, type Response } from "express"
 import {Pool} from "pg"
+import config from "./config"
 const app : Application = express()
-const port = 5000
+const port = config.port
 
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended: true}))
 
 const pool = new Pool({
-    connectionString: "postgresql://neondb_owner:npg_P8KyzHbFXk9m@ep-divine-bird-aqjjmmbm-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    connectionString: config.connection_string
 })
 
 const initDB = async()=>{
