dc854327a0ce76cba2f58e0361540c6a1c695562
Ahnaf Abid <ahnafabid600@gmail.com>
2026-06-09 18:54:55 +0600

    Add CORS support and global error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import { authRouter } from "./modules/auth/auth.route"
 import fs from "fs"
 import logger from "./middleware/logger"
 import CookieParser from 'cookie-parser'
+import cors from "cors"
+import globalErrorHandler from "./middleware/globalErrorHandler"
 
 const app : Application = express()
 
@@ -15,8 +17,10 @@ app.use(CookieParser())
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended: true}))
-
 app.use(logger);
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 app.get('/', (req : Request, res : Response) => {
 //   res.send('Hello World!')
@@ -30,6 +34,8 @@ app.use('/api/users', userRouter)
 app.use('/api/profile', profileRoutes)
 app.use('/api/auth', authRouter)
 
+
+app.use(globalErrorHandler);
 
 
 export default app
